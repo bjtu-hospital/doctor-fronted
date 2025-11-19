943638a50603782af85d8e07adbddbf06f7fcf7c
wyq <1903910367@qq.com>
2025-11-19 15:40:08 +0800

    1.新增了uView的组件库 2.新增了toast函数和组件,在utils下.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import {
 	createSSRApp
 } from "vue";
 import App from "./App.vue";
+import uviewPlus from 'uview-plus';
+
 export function createApp() {
 	const app = createSSRApp(App);
+
+	app.use(uviewPlus);
+
 	return {
 		app,
 	};
